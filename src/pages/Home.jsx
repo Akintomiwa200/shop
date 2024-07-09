558d344b87acabcd4831115c5e2ad826f2a283bd
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-10 00:26:21 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/prod/Header2'
-// import Header from '../components/Header'
+//import Header from '../components/prod/Header2'
+import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import ProductDisplay from '../components/ProductDisplay'
