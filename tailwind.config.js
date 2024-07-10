e790cd33ab9ccb0731f95128a2ea6a4fe7c5a7fb
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-10 01:38:18 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        libre: ['"inter"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
