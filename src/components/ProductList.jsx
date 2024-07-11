b9f3c5ad6565418a05893a31a5da14b49836b640
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-11 18:28:51 +0100

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -34,18 +34,18 @@ const ProductList = () => {
   };
 
   return (
-    <div className="grid w-[80vw] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid w-[90vw] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center">
       {products.map((product, index) => (
-        <div key={product.id} className="p-4">
+        <div key={product.id} className="p-4 w-[40vw]">
           <img src={product.imageUrl} alt={product.name} className="w-full h-64 object-cover" />
-          <h2 className="text-xl font-bold mt-2">{product.name}</h2>
-          <p>{product.description}</p>
+          <h2 className="text-xs md:text-xl font-bold mt-2">{product.name}</h2>
+          <p className='text-xs lg:xl'>{product.description}</p>
           <div className="flex items-center gap-4 mt-2">
-            {renderRating(product.rating)}<span className='gap-2'>{product.rating}({product.reviews})</span>
+            {renderRating(product.rating)}<span className='gap-2 text-xs'>{product.rating}({product.reviews})</span>
           </div>
           <span className='flex items-center justify-between'>
-            <span className="text-lg font-semibold">₦{product.price}</span>
-            <div className="flex items-center mt-2 text-xs md:text-xl">
+            <span className="text-xs lg:text-lg font-semibold">₦{product.price}</span>
+            <div className="flex items-center mt-2 text-xs text-xs md:text-xl">
               <button
                 onClick={() => decrementQuantity(index)}
                 className="pl-1 bg-gray-200 rounded-full text-gray-800 text-xs md:text-xl hover:bg-gray-300">
