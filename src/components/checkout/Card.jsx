12179d1ba3cfab003ba530cfeff26c7415db5ec9
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-14 14:35:53 +0100

    Update Card.jsx

--- src/components/checkout/Card.jsx
+++ src/components/checkout/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = ({ number, name, expiry }) => {
     return (
-        <div className="w-[300px] h-[180px] bg-gray-200 rounded-lg p-4 shadow-md relative text-gray-800">
+        <div className="w-[300px] h-[180px] bg-blue-700 rounded-lg p-4 shadow-md relative text-gray-800">
             <div className="text-right text-sm">{expiry}</div>
             <div className="text-center text-xl mt-4">{number}</div>
             <div className="absolute bottom-4 left-4 text-sm">{name}</div>
