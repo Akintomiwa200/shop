a7ed964e8989511ef2bd2677abf5d7ab1baba41a
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-15 08:03:12 +0100

    Update Cart.jsx

--- src/components/prod/Cart.jsx
+++ src/components/prod/Cart.jsx
@@ -12,7 +12,13 @@ const Cart = () => {
                     <img src={item.image} alt={item.name} className="w-16 h-16 object-cover rounded" />
                     <div className="ml-4 flex-1">
                         <h3 className="text-lg font-bold">{item.name}</h3>
-                        <div className="flex gap-2 mt-2">
+                     
+                        <p className="text-lg font-semibold mt-2">₦{item.price * item.quantity}</p>
+                        <div className="mt-2">
+                            <button onClick={() => handleRemoveItem(index)} className="px-4 py-2 bg-red-500 text-white rounded mr-2">Remove</button>
+                        </div>
+                        
+                           <div className="flex gap-2 mt-2">
                             <button onClick={() => handleQuantityChange(index, -1)} className="p-2 bg-gray-200 rounded-full text-gray-800 hover:bg-gray-300">
                                 <FaMinus />
                             </button>
@@ -20,11 +26,6 @@ const Cart = () => {
                             <button onClick={() => handleQuantityChange(index, 1)} className="p-2 bg-gray-200 rounded-full hover:bg-gray-300 text-gray-800">
                                 <FaPlus />
                             </button>
-                        </div>
-                        <p className="text-lg font-semibold mt-2">₦{item.price * item.quantity}</p>
-                        <div className="mt-2">
-                            <button onClick={() => handleRemoveItem(index)} className="px-4 py-2 bg-red-500 text-white rounded mr-2">Remove</button>
-                            <button className="px-4 py-2 bg-blue-500 text-white rounded">Save for Later</button>
                         </div>
                     </div>
                 </div>
