036eb6495d3db7ff2c26a082ed4b06b35bc62a18
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2025-03-22 01:24:40 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.timbu.cloud',
+        target: 'https://shoppy-pzzi.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
